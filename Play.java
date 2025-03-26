0d2da13110a579c39eb4e6e6a9fb6db607c87148
Gabriel Salinas-Carter <gjcarter9@gmail.com>
2025-03-25 19:47:32 -0700

    Add files via upload

---
+++ Play.java
@@ -0,0 +1,13 @@
+public class Play {
+    public static void welcome(){
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Welcome! thank you for choosing our program.");
+        System.out.println("You will be given the choice to play either a guessing game or rock, paper, scissors.");
+        System.out.println("Please choose which ever one you would like and play for as long as you want.");
+        System.out.println("Good luck and have fun!");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+    public static void main(String[] args) {
+        welcome();
+    }
+}
